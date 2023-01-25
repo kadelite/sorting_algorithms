55574af500c67cc061616892e3f3b2a2704b9dbb
kadelite <mechatronics.samson@gmail.com>
2023-01-25 12:02:23 -0800

    task together

---
+++ 3-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    /*int array[] = {19, 0, 1, 33, 23, 12, 17, 65, 43, 21, 76};*/
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    quick_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
+}
